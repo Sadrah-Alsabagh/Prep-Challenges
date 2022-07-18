59cb5906f7698a461860a0df83fecd8cf37d0207
Sadrah-alsabagh <sedrasabbagh33@gmail.com>
2022-07-18 10:22:31 +0300

    solve challenge2

--- code-challenges/challenges-01.js
+++ code-challenges/challenges-01.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//const { number } = require("yargs");
+
 // --------------------------------------------------------------------------------------------------------
 //  Challenge 01:
 //  Required:
@@ -7,7 +9,7 @@
 //  Write a function that takes an array and returns the maximum value in the provided array
 //  without using build-in functions.
 //
-let arr= [12, 32, 22, 45, 78, 12, 50]
+//let arr= [12, 32, 22, 45, 78, 12, 50]
 // Output: 78
 
 const findMax = (arr) => {
@@ -30,13 +32,18 @@ const findMax = (arr) => {
 //  Write a function that takes an arrays and returns the Sum of the numeric values
 //  inside the array (there could be strings inside), without using built-in functions
 //
-// Input: [20, '234', 'car', 41, 20, 'chair']
+ let arr = [20, '234', 'car', 41, 20, 'chair']
 // Output: 81
 const sumNums = (arr) => {
-  let sum;
+  let sum =0;
   // write your code here
-  return sum;
-};
+  
+  for (let i = 0; i < arr.length; i++) {  
+    if(typeof arr[i] === 'number' ){
+    sum += arr[i]}
+  }
+ // return sum;
+ console.log(sum);
+};sumNums(arr)
 // -------------------------------------------------------------------------------------------------------
-
-module.exports = { findMax, sumNums };
+//module.exports = { findMax, sumNums };
